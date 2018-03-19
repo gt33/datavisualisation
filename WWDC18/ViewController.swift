23064baa13d912e341a8312b23d40b9ff064e165
George Taylor <gt1porsche@10-20-50-159.0200.fed.it.iosda.org>
2018-03-19 15:12:49 +0100

    Initial Commit

---
+++ WWDC18/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WWDC18
+//
+//  Created by G on 19/03/2018.
+//  Copyright © 2018 G. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
